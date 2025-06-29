48af105c49ef1a7d90e5047f951c9df71c25dfb0
Christina O'Donnell <cdo@mutix.org>
2025-06-29 10:32:10 +0100

    Restructure packages.nix.

--- nix/packages.nix
+++ nix/packages.nix
@@ -12,14 +12,11 @@
         type = lib.types.anything;
       };
       config.packages = rec {
-        agda = pkgs.agda.withPackages (ps: [
+        inherit (inputs.onelab.packages.${system}) _1lab Agda agda2-mode;
+        agda-base = inputs.onelab.packages.${system}.agda;
+        agda = agda-base.withPackages (ps: [
           inputs.onelab.packages.${system}._1lab
-          #pkgs.agdaPackages.standard-library
         ]);
-        inherit (inputs.onelab.packages.${system}) _1lab;
-        #inherit (pkgs.agdaPackages) _1lab;
-        inherit (pkgs.labHaskellPackages) Agda;
-        inherit (pkgs.emacsPackages) agda2-mode;
         just-agda = inputs.just-agda.packages.${pkgs.system}.default.override {
           inherit agda agda2-mode;
         };
