c0763b8305a4596a8a4cbae90035dcd706cb363a
Christina O'Donnell <cdo@mutix.org>
2025-06-30 22:10:17 +0100

    nix: Add ghc.

--- nix/packages.nix
+++ nix/packages.nix
@@ -12,6 +12,7 @@
         type = lib.types.anything;
       };
       config.packages = rec {
+        ghc = pkgs.ghcWithPackages (p: with p; [ ieee754 ]);
         inherit (inputs.onelab.packages.${system}) _1lab Agda agda2-mode;
         agda-base = inputs.onelab.packages.${system}.agda;
         agda = agda-base.withPackages (ps: [
