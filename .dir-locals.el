2c64b4af2614cef22ff1823fcb098eb8cecb39c8
Christina O'Donnell <cdo@mutix.org>
2025-07-06 20:27:10 +0100

    wip Try adding .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+((agda2-mode . (agda-input-user-translations .
+  '(("^-1" . ("⁻¹"))
+    ("sym" . ("˘"))
+    ("foo" . ("~"))
+    ("\\\\" . ("∖")))))) 
+
+; ((nil . ((eval . (setq agda-input-user-translations
+;                       '(("^-1" . ("⁻¹"))
+;                         ("sym" . ("˘"))
+;                         ("foo" . ("~"))
+;                         ;; Triple backslash -> \setminus
+;                         ("\\\\" . ("∖")))))))
