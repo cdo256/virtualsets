59de5815142dd14a17034bf15396bd3ef0d7d80e
Christina O'Donnell <cdo@mutix.org>
2025-07-20 13:09:04 +0100

    flake: Update.

--- nix/packages.nix
+++ nix/packages.nix
@@ -4,7 +4,7 @@
     { system, pkgs, ... }:
     {
       config.packages = rec {
-        agda-base = inputs.onelab.packages.${system}.agda;
+        # agda-base = inputs.onelab.packages.${system}.agda;
         agda = inputs.agda-cubical.packages.${system}.agdaWithCubical;
         just-agda = inputs.just-agda.packages.${system}.default.override {
           inherit agda;
