07c77b6b07391e358534a6ee6bea2ab149a60925
Christina O'Donnell <cdo@mutix.org>
2025-07-22 21:27:32 +0100

    flake: Add standard-library.

--- nix/packages.nix
+++ nix/packages.nix
@@ -4,8 +4,11 @@
     { system, pkgs, ... }:
     {
       config.packages = rec {
-        # agda-base = inputs.onelab.packages.${system}.agda;
-        agda = inputs.agda-cubical.packages.${system}.agdaWithCubical;
+        agda-base = pkgs.agda;
+        agda = agda-base.withPackages (ps: [
+          ps.standard-library
+          ps.cubical
+        ]);
         just-agda = inputs.just-agda.packages.${system}.default.override {
           inherit agda;
           inherit (pkgs.emacsPackages) agda2-mode;
